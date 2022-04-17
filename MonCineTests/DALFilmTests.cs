868f7825d14e16e3e88ef17a4edb205a007a80f6
Ahmed Toumi <ahmed.toumi2002@gmail.com>
2022-04-17 09:41:19 -0400

    Nouvelle version du projet test

---
+++ MonCineTests/DALFilmTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace MonCineTests
+{
+    public class DALFilmTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int test = 20;
+
+
+            Assert.Equal(20, test);
+        }
+    }
+}
