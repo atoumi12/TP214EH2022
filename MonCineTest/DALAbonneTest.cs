78d23396bb644647614f8f9b3b293f85a175c8c7
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2022-04-17 12:20:05 -0400

    nouveau tst

---
+++ MonCineTest/DALAbonneTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MonCineTest
+{
+    [TestClass]
+    public class DALAbonneTest
+    {
+        [TestMethod]
+        public void DALAbonneCanReadObject()
+        {
+            
+        }
+    }
+}
